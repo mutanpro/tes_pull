b3e7866c3398bad8abe4cc9bc3a76af8908cd723
mutanpro <mutanpro@gmail.com>
2020-06-05 00:17:35 +0700

    tambah gitignior

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
-// exec("git pull ssh://git@git.unisnu.ac.id:8022/mutanpro/sapmb-unisnu.git master");
-$output = shell_exec("git clone https://mutanpro:mutanpro@git.unisnu.com:mutanpro/sapmb-unisnu.git");
-echo "<pre>$outputs</pre>";
+// // exec("git pull ssh://git@git.unisnu.ac.id:8022/mutanpro/sapmb-unisnu.git master");
+// $output = shell_exec("git clone https://mutanpro:mutanpro@git.unisnu.com:mutanpro/sapmb-unisnu.git");
+// echo "<pre>$outputs</pre>";
+// $command = 'git fetch';
+// $output = exec($command, $array_output, $ret_val);
+// var_dump($ret_val);
+// var_dump($array_output);
+// var_dump($output);
+// echo "<pre>$output</pre>";
+echo shell_exec("git fetch");
