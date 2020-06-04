b9f96bbff247f618dce21c78155b2016455eaddd
mutanpro <mutanpro@gmail.com>
2020-06-05 00:08:40 +0700

    tes

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// exec("git pull ssh://git@git.unisnu.ac.id:8022/mutanpro/sapmb-unisnu.git master");
+$output = shell_exec("git clone https://mutanpro:mutanpro@git.unisnu.com:mutanpro/sapmb-unisnu.git");
+echo "<pre>$outputs</pre>";
